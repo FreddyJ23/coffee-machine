1fe9f2f312491d8c46ec6327cb4985ff6b3608e5
freddy <freedomochulor22gmail.com>
2024-07-31 17:45:44 +0100

    second commit

--- main.py
+++ main.py
@@ -30,56 +30,72 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
-WATER = resources["water"]
-MILK = resources["milk"]
-COFFEE = resources["coffee"]
-COST = 0
-# TODO: 3. Print report
 
-
-def report(water, milk, coffee, money):
-    water_left = WATER - water
-    milk_left = MILK - milk
-    coffee_left = COFFEE - coffee
-    money_left = COST + money
-    return f"Water: {water_left}ml\nMilk: {milk_left}ml\nCoffee: {coffee_left}g\nMoney: ${money_left}"
+profit = 0
 
 
 # TODO: 4. Check if resources is sufficient to make drink
-    ## create a function collect ingredents from menu and check if they check are above current reources
-def is_enough(coffee_type):
-    coffee_water = MENU[coffee_type]["ingredients"]["water"]
-    coffee_milk = MENU[coffee_type]["ingredients"]["milk"]
-    coffee_coffee = MENU[coffee_type]["ingredients"]["coffee"]
-    resource_water = resources["water"]
-    resource_milk = resources["milk"]
-    resource_coffee= resources["coffee"]
-    if coffee_water > resource_water:
-       print("Sorry there is not enough water.")
-    if coffee_milk > resource_milk:
-       print("Sorry there is not enough milk.")
-    if coffee_coffee > resource_coffee:
-       print("Sorry there is not enough coffee.")
 
 
-# print(report(20, 0, 0, 0))
-
-# TODO: 1. Prompt the User for coffee choice
-reply = input("What would you like? (expresso/latte/cappuccino): ").lower()
-
-# TODO: 2. Turn off the machine
-
-is_enough(reply)
-
+def is_enough(coffee_ingredients):
+    """returns true or false if items are sufficient to make order"""
+    for item in coffee_ingredients:
+        if coffee_ingredients[item] > resources[item]:
+            print(f"Sorry, not enough {item}")
+            return False
+    return True
 
 
+# TODO: 5. Process coins
 
 
+def coins():
+    """calculate total amount of coins put in"""
+    quarter = int(input("How many quarters? ")) * 0.25
+    dime = int(input("How many dimes? ")) * 0.1
+    nickel = int(input("How many nickels? ")) * 0.05
+    penny = int(input("How many pennies? ")) * 0.01
+    return quarter + dime + nickel + penny
 
 
+# TODO: 6. Check if transaction is successful
+def is_transaction_successful(user_money, coffee_cost):
+    """Checks if transaction is successful by returning an addition to profit"""
+    if user_money >= coffee_cost:
+        change_left = round(user_money - coffee_cost, 2)
+        print(f"Here is ${change_left} in change.")
+        global profit
+        profit += coffee_cost
+        return True
+    else:
+        print("Sorry not enough money. Here is your money back")
+        return False
 
 
+# TODO: 7. Make coffee and deduct from resources
 
 
+def make_coffee(choice, coffee_ingredients):
+    for item in coffee_ingredients:
+        resources[item] -= coffee_ingredients[item]
+    print(f"Here is your {choice}☕. Enjoy!")
 
 
+is_continue = True
+while is_continue:
+
+    # TODO: 1. Prompt the User for coffee choice
+    reply = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    # TODO: 2. Turn off the machine
+    if reply == "off":
+        is_continue = False
+    # TODO: 3. Print report
+    elif reply == "report":
+        print(
+            f"Water: {resources["water"]}ml\nMilk: {resources["milk"]}ml\nCoffee: {resources["coffee"]}g\nMoney: ${profit}")
+    else:
+        drink = MENU[reply]
+        if is_enough(drink["ingredients"]):
+            user_payment = coins()
+            if is_transaction_successful(user_payment, drink["cost"]):
+                make_coffee(reply, drink["ingredients"])
